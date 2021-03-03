ef7c5d248b5d227cc52504eed702ecfb7b1593ac
Christian Anderson <40026509+ChristianFlows@users.noreply.github.com>
2021-03-03 11:03:26 -0500

    added enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerHealth, playerAttack)
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 // Alert players that they are starting the round
 window.alert("Welcome to Robot Gladiators!")
 //Prompt to fight or skip
@@ -62,4 +62,8 @@ if (confirmSkip) {
 }
     };
 
-fight();
+// fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i])
+}
